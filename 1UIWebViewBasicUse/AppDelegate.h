380db0934a476acd0eb80d99ff3bc7d44dbf4e54
Asuka <154906186@qq.com>
2016-04-10 16:57:42 +0800

    Initial Commit

---
+++ 1UIWebViewBasicUse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1UIWebViewBasicUse
+//
+//  Created by  a on 16/4/10.
+//  Copyright © 2016年 eva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

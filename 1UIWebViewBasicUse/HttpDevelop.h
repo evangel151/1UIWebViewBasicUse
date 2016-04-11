98c00aa26578d30da3843ded1b5857ae29fe6353
Asuka <154906186@qq.com>
2016-04-11 23:06:52 +0800

    basic use of webView
    basic use of webView

---
+++ 1UIWebViewBasicUse/HttpDevelop.h
@@ -0,0 +1,21 @@
+//
+//  HttpDevelop.h
+//  1UIWebViewBasicUse
+//
+//  Created by  a on 16/4/10.
+//  Copyright © 2016年 eva. All rights reserved.
+//
+
+#ifndef HttpDevelop_h
+#define HttpDevelop_h
+
+
+#endif /* HttpDevelop_h */
+
+网页开发相关
+三大要素
+* HTML // 网页基本内容
+* CSS  // 美化网页
+* JavaScript // 用户交互相关
+
+1. HTML 超文本标记语言
--- 1UIWebViewBasicUse/ViewController.m
+++ 1UIWebViewBasicUse/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -16,12 +17,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+     NSLog(@"加载完毕——————");
+    
+    // 直接使用webView空间加载一个资源 (可以加载网页 txt mp4 ppt等多种格式的资源)
+    // 1. 创建一个简单的url请求
+    // * 加载mp4视频文件 (远程网页)
+    // NSURL *url = [NSURL URLWithString:@"http://localhost:8080/MJServer/resources/videos/minion_16.mp4"];
+    // * 加载txt文件 (加载pptx文件时无法正常显示)
+    // NSURL *url = [[NSBundle mainBundle] URLForResource:@"关于" withExtension:@"txt"];
+    // * 加载本地网页 
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"salary" withExtension:@"html"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    // 2. 根据请求区加载对应的资源
+    [self.webView loadRequest:request];
+    
+    // 3. 使webView内资源适配屏幕
+    [self.webView scalesPageToFit];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
